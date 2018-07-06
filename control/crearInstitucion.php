6b6f39dc84a700da303b5eac4fba9067866ce030
Liliana <Liliana@DESKTOP-MGG7HL0>
2018-07-06 01:45:07 -0500

    Crear institucion

---
+++ control/crearInstitucion.php
@@ -0,0 +1,30 @@
+<?php
+
+include '../conexion.php';
+
+//reciviendo datos del formulario
+$nom=$_POST['nombreIns'];
+$dir=$_POST['direccionIns'];
+$tel = $_POST['telefonoIns'];
+$correo=$_POST['correoIns'];
+    
+
+
+if (!isset($nom) || !isset($dir) || !isset($tel) || !isset($correo)) {
+    header("location:../page-crear-institucion.php");
+}
+
+$insertar="INSERT INTO institucion(InstitucionId,InstitucionNombre, InstitucionDireccion, InstitucionTelefono, InstitucionCorreo) VALUES ('$nom','$dir', '$tel', '$correo' )";
+
+if($conexion->query($insertar)==TRUE){
+    echo '1';
+    //echo "Registro exitoso";
+        //header("location:../page-asignar-permisos-user.php?usuario=$user"."&nombre=$nombre"."&apellidos=$apellidos");
+}else{
+    echo '0';
+    //echo "Error, nombre de usuario existente";
+}
+$conexion->close();
+
+
+
