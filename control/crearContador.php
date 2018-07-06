b12a5c30d606576edcac2e631cf0a00e48f8666a
Liliana <Liliana@DESKTOP-MGG7HL0>
2018-07-06 01:28:07 -0500

    Crear contador

---
+++ control/crearContador.php
@@ -0,0 +1,27 @@
+<?php
+
+include '../conexion.php';
+
+//recibiendo datos del formulario
+$dni = $_POST['dniConta'];
+$nombre = $_POST['nombreConta'];
+$ape = $_POST['apellidoConta'];
+$telefono = $_POST['telefonoConta'];
+$email = $_POST['emailConta'];
+
+
+if (!isset($dni) ||  !isset($nombre)|| !isset($ape) || !isset($telefono) || !isset($email)) {
+    header("location:../page-crear-contador.php");
+}
+
+$insertar="INSERT INTO contador(ContadorDni,ContadorNombre, ContadorApellidos,ContadorCorreo, ContadorTelefono) VALUES ('$dni','$nombre','$ape','$email','$telefono')";
+
+if($conexion->query($insertar)==TRUE){
+    echo '1';
+    //echo "Registro exitoso";
+        //header("location:../page-asignar-permisos-user.php?usuario=$user"."&nombre=$nombre"."&apellidos=$apellidos");
+}else{
+    echo '0';
+    //echo "Error, nombre de usuario existente";
+}
+$conexion->close();
--- page-crear-contador.php
+++ page-crear-contador.php
@@ -5,32 +5,12 @@ if (!isset($_SESSION['usuario'])) {
     //si no hay sesion activa 
     header("location:index.php");
 } else {
-    include 'conexion.php';
-    $id = $_GET['id'];
-    if (!isset($id)) {
-        header("location:page-ver-escuela.php");
-    }
-    $buscar = "SELECT * FROM escuela WHERE EscuelaId='$id'";
-    $resultado = $conexion->query($buscar);
-    if ($resultado->num_rows === 0) {
-        header("location:page-ver-escuela.php");
-    }
-    $provBD = $resultado->fetch_assoc();
-    
-    $buscarDir = "SELECT * FROM director";
-    $resultDir = $conexion->query($buscarDir);
-    $buscarSec = "SELECT * FROM secretaria";
-    $resultSec = $conexion->query($buscarSec);
     ?>
-
-
-
-
     <!DOCTYPE html>
     <html lang="es">
 
         <head>
-            <title>Configurar Escuela</title>
+            <title>Crear Contador</title>
             <!--Let browser know website is optimized for mobile-->
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
             <!-- Favicons-->
@@ -49,7 +29,7 @@ if (!isset($_SESSION['usuario'])) {
 
         </head>
 
-        <body>
+        <body onload="focus();">
 
             <!-- START MAIN -->
             <div id="main">
@@ -70,12 +50,12 @@ if (!isset($_SESSION['usuario'])) {
                             <div class="container">
                                 <div class="row">
                                     <div class="col s12 m12 l12">
-                                        <h5 class="breadcrumbs-title">Configurar Escuela</h5>
+                                        <h5 class="breadcrumbs-title">Crear Contador</h5>
                                         <ol class="breadcrumb">
-                                            <li class=" grey-text lighten-4">Gestion de Escuela
+                                            <li class=" grey-text lighten-4">Gestion de Usuarios
                                             </li>
-                                            <li class="grey-text lighten-4" >Ver Escuela</li>
-                                            <li class="active blue-text">Configurar Escuela</li>
+                                            <li class="active blue-text" >Crear Contador</li>
+
                                         </ol>
 
                                     </div>
@@ -90,74 +70,58 @@ if (!isset($_SESSION['usuario'])) {
                                 <div class="col s12 m12 l12">
                                     <div class="section">
                                         <div id="roboto">
-                                            <h4 class="header">Configuracion de Escuela</h4>
+                                            <h4 class="header">Creación de datos del Contador</h4>
                                             <p class="caption">
-                                                En este panel usted podra hacer la configuracion de la escuela.
+                                                En este panel usted podra crear nuevos datos para la contadora
                                             </p>
                                             <div class="divider"></div>
                                             <div class="row">
                                                 <!-- Form with validation -->
                                                 <div class="col offset-l2 s12 m12 l8">
                                                     <div class="card-panel">
-                                                        <h4 class="header2">Escuela: <?php echo $provBD['EscuelaId']; ?></h4>
+                                                        <h4 class="header2">Nuevo Contador</h4>
                                                         <div class="row">
-                                                            <form id="configurar" class="col s12" action="control/modificarEscuela.php" method="POST">
+                                                            <form id="create" class="col s12" action="control/crearContador.php" method="POST">
+
                                                                 <div class="row">
                                                                     <div class="input-field col s12">
-                                                                        <input id="username" type="text" class="validate" name="id" required="" hidden="true" value="<?php echo $provBD['EscuelaId']; ?>">
-
+                                                                        <input id="dni" type="text" class="validate" name="dniConta" required="">
+                                                                        <label for="dni">DNI:</label>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="row">
+                                                                    <div class="input-field col s12">
+                                                                        <input id="nombre" type="text" class="validate" name="nombreConta" required="">
+                                                                        <label for="nombre">Nombre:</label>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="row">
+                                                                    <div class="input-field col s12">
+                                                                        <input id="apellido" type="text" class="validate" name="apellidoConta" required="">
+                                                                        <label for="apellido">Apellido:</label>
                                                                     </div>
                                                                 </div>
                                                                 
                                                                 <div class="row">
                                                                     <div class="input-field col s12">
-                                                                        <input id="nombre" type="text" class="validate" name="nombreescuela" required="" value="<?php echo $provBD['EscuelaNombre']; ?>">
-                                                                        <label class="active" for="nombre">Nombre:</label>
+                                                                        <input id="email" type="text" class="validate" name="emailConta" required="">
+                                                                        <label for="email">Email:</label>
                                                                     </div>
                                                                 </div>
                                                                 
-                                                                                                                             
                                                                 <div class="row">
                                                                     <div class="input-field col s12">
-                                                                        <input id="telefono" type="text" class="validate" name="telefonoescuela" required="" value="<?php echo $provBD['EscuelaTelefono']; ?>">
-                                                                        <label class="active" for="telefono">Telefono:</label>
+                                                                        <input id="telefono" type="text" class="validate" name="telefonoConta" required="">
+                                                                        <label for="telefono">Telefono:</label>
                                                                     </div>
                                                                 </div>
                                                                 
-                                                                <div class="col s12 m6 l6">
-                                                                        <label >Director:</label>
-                                                                        <select id="dir" class="browser-default" name="directorescuela" required="">
-                                                                            <option value="" disabled selected>Selecciona</option>
-                                                                            <?php while ($row = $resultDir->fetch_assoc()) { ?>
-                                                                                <option value="<?php echo $row['DirectorDni']; ?>"><?php echo $row['DirectorNombre']; ?></option>
-                                                                            <?php }
-                                                                            ?>
-
-                                                                        </select>
-                                                                    </div>
-                                                                
-                                                                </div>
-                                                        
-                                                                <div class="col s12 m6 l6">
-                                                                        <label >Secretaria:</label>
-                                                                        <select id="sec" class="browser-default" name="secretariaescuela" required="">
-                                                                            <option value="" disabled selected>Selecciona</option>
-                                                                            <?php while ($row = $resultSec->fetch_assoc()) { ?>
-                                                                                <option value="<?php echo $row['SecretariaDni']; ?>"><?php echo $row['SecretariaNombre']; ?></option>
-                                                                            <?php }
-                                                                            ?>
-
-                                                                        </select>
-                                                                </div>
-                                                                
-                                                                </div>
-                                                        
                                                                 <br>
                                                                 <div class="divider"></div>
                                                                 <div class="row">
                                                                     <div class="input-field col s12">
-                                                                        <button class="btn cyan waves-effect waves-light right" type="submit" name="action">Guardar Cambios
-                                                                            <i class="mdi-content-save left"></i>
+                                                                        <button class="btn cyan waves-effect waves-light right" type="submit" name="action">Registrar
+                                                                            <i class="mdi-image-edit left"></i>
                                                                         </button>
                                                                     </div>
                                                                 </div>
@@ -172,26 +136,28 @@ if (!isset($_SESSION['usuario'])) {
                                 </div>
                             </div>
 
+
                         </div>
                         <!--end container-->
-                        <!--modal correcto-->
+
+                        <!--modal error-->
                         <div id="modal1" class="modal">
                             <div class="modal-content">
-                                <h4 class="green-text">EXITO!!!</h4>
-                                <p> Escuela modificada correctamente.</p>
+                                <h4 class="red-text">ERROR!!!</h4>
+                                <p>Contador no creado correctamente.</p>
                             </div>
                             <div class="modal-footer">
-                                <a href="page-ver-escuela.php" class="modal-action modal-close waves-effect waves-green btn-flat">Aceptar</a>
+                                <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Aceptar</a>
                             </div>
                         </div>
                         <!--modal error-->
                         <div id="modal2" class="modal">
                             <div class="modal-content">
-                                <h4 class="red-text">ERROR!!!</h4>
-                                <p>La escuela no pudo ser modificada, intentelo de nuevo.</p>
+                                <h4 class="green-text">EXITO!!!</h4>
+                                <p>Contador creado correctamente.</p>
                             </div>
                             <div class="modal-footer">
-                                <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Aceptar</a>
+                                <a href="page-crear-contador.php" class="modal-action modal-close waves-effect waves-green btn-flat">Aceptar</a>
                             </div>
                         </div>
                     </section>
@@ -215,7 +181,7 @@ if (!isset($_SESSION['usuario'])) {
             <!-- //////////////////////////////////////////////////////////////////////////// -->
 
             <!-- START FOOTER -->
-    <?php include 'inc/footer.inc'; ?>
+            <?php include 'inc/footer.inc'; ?>
             <!-- END FOOTER -->
 
 
@@ -224,8 +190,7 @@ if (!isset($_SESSION['usuario'])) {
             ================================================ -->
 
             <!-- jQuery Library -->
-            <script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>  
-
+            <script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>    
             <!--materialize js-->
             <script type="text/javascript" src="js/materialize.js"></script>
             <!--scrollbar-->
@@ -235,42 +200,40 @@ if (!isset($_SESSION['usuario'])) {
             <script type="text/javascript" src="js/plugins.js"></script>
 
             <script>
-                $(document).ready(function () {
-                    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-                    
-                    $('#modal1').modal();
-                    $('#modal2').modal();
-                });
-                var frm = $('#configurar');
+            $(document).ready(function () {
+                // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
 
-                frm.submit(function (ev) {
-                    ev.preventDefault();
-                    $.ajax({
-                        type: frm.attr('method'),
-                        url: frm.attr('action'),
-                        data: frm.serialize(),
-                        success: function (respuesta) {
-                            if (respuesta == 1) {
-                                $('#modal1').openModal();
-                            } else {
+                $('#modal2').modal();
+                $('#modal1').modal();
+            });
 
-                                $('#modal2').openModal();
-                            }
+            var frm = $('#create');
+            frm.submit(function (ev) {
+                ev.preventDefault();
+                $.ajax({
+                    type: frm.attr('method'),
+                    url: frm.attr('action'),
+                    data: frm.serialize(),
+                    success: function (respuesta) {
+                        if (respuesta == 1) {
+                            //$('#modal2').openModal();
+                            //document.location.href = "page-crear-proveedor.php";
+    //                                location.reload();
+                            $('#modal2').openModal();
+
+                        } else {
+
+                            $('#modal1').openModal();
                         }
-                    });
-
-
+                    }
                 });
-            </script>
 
+
+            });
+            </script>
         </body>
 
     </html>
     <?php
 }
 ?>
-
-
-
-
-
