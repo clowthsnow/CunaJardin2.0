3489550b8c40ef3b1fd1b120930351a28a6603d9
Mario-Snow <salazarmariot@gmail.com>
2018-07-06 01:17:36 -0500

    TipoConcepto

---
+++ control/crearTipoConcepto.php
@@ -0,0 +1,29 @@
+<?php
+
+include '../conexion.php';
+
+//reciviendo datos del formulario
+
+$nombre = $_POST['nombretipo'];
+
+
+if (  !isset($nombre)) {
+    header("location:../page-crear-tipoConcepto.php");
+}
+
+$insertar="INSERT INTO tipoconcepto( TipoConceptoDetalle) VALUES ('$nombre')";
+
+if($conexion->query($insertar)==TRUE){
+    echo '1';
+    //echo "Registro exitoso";
+        //header("location:../page-asignar-permisos-user.php?usuario=$user"."&nombre=$nombre"."&apellidos=$apellidos");
+}else{
+    echo '0';
+    //echo "Error, nombre de usuario existente";
+}
+$conexion->close();
+
+
+
+
+
---
+++ control/modificarTipoConcepto.php
@@ -0,0 +1,18 @@
+<?php
+
+include '../conexion.php';
+
+//reciviendo datos del formulario
+$id = $_POST['id'];
+$nombre = $_POST['nombretipo'];
+
+if (!isset($id) || !isset($nombre) ) {
+    header("location:../page-ver-tipoConceptos.php");
+}
+$actualiza="UPDATE tipoconcepto SET TipoConceptoDetalle='$nombre' WHERE TipoConceptoId='$id'";
+if($conexion->query($actualiza) === TRUE){
+    echo '1';
+}else{
+    echo '0';
+}
+$conexion->close();
