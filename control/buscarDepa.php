6ca641c6f959e1098a4b2e3b091adff6644d76a7
Mario-Snow <salazarmariot@gmail.com>
2018-11-08 22:15:56 -0500

    avance miercoles 8/11

--- control/buscarDepa.php
+++ control/buscarDepa.php
@@ -11,6 +11,6 @@ $salida = "<option value=\"\" disabled selected>Provincia :*</option>";
 while ($fila = $resultado->fetch_assoc()) {
     $salida .= "<option value=\"".$fila['id']."\">".$fila['name']."</option>";
 }
-echo $salida;
+echo utf8_encode($salida);
 
 $conexion->close();
